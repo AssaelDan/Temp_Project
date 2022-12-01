0a60d74d84966d656341c3cd0af2a986e22033e5
AssaelDan <assaeldan@gmail.com>
2022-12-02 01:04:25 +0200

    added a basic python script

---
+++ python/test.py
@@ -0,0 +1,5 @@
+import unreal
+
+unreal.log("*************************************************")
+unreal.log("This print came from the IDE")
+unreal.log("*************************************************")
